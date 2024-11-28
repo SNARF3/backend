5f185756df2bf9ca0a9c6f209a256c35e76e3334
SNARF3 <snarfeo@gmail.com>
2024-11-28 03:58:05 -0400

    xd

---
+++ src/routes/proyectos.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { proyectoController} from "../controllers/proyectos.controllers.js";
+
+const router = Router();
+
+
+router.post("/Proyectos",proyectoController.BuscarProyectos)
+
+export default router;
